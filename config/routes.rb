ed692b9bde2458c29c013f8a9bc9f4f488d91950
mom0tomo <momo76w@gmail.com>
2018-09-18 23:59:50 +0900

    User新規登録機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   root to: 'top#index'
+
+  get 'signup', to: 'users#new'
+  post 'signup', to: 'users#create'
+  resources :users, only: [:index, :show, :new, :create]
 end
